77051d98c355d364110efea0bd60e083fd11f27f
Ibrahima Sarr <ibrahimasarr88@gmail.com>
2016-03-20 00:41:29 -0400

    add root

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id:1,
+	description:'Meet mom for lunch',
+	completed: false
+},{
+	id:2,
+	description:'Go to market',
+	completed:false
+},{
+	id:3,
+	description:'Go to market 2',
+	completed:true
+}]
 
 app.get('/',function(req,res){
 	res.send('Todo API Root');
+});
+
+app.get('/todos',function(req,res){
+	res.json(todos);
 });
 
 app.listen(PORT, function(){
